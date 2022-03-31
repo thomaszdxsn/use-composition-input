127fcdc8bf1caf361d824044db3804f16b320daf
Yang,Zhou <thomaszdxsn@icloud.com>
2022-03-31 16:58:24 +0800

    :sparkles: finish main function

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # use-composition-input
+
+> The `onChange` should not emit before composition end
+
+```typescript
+function Demo() {
+  const callbacks = useCompositionInput((e) => {
+    console.log(e);
+  }, []);
+  return <input {...callbacks} />;
+}
+```
---
+++ src/index.ts
@@ -0,0 +1,39 @@
+import {
+  ChangeEventHandler,
+  CompositionEventHandler,
+  DependencyList,
+  useMemo,
+  useRef,
+} from "react";
+
+interface Callbacks {
+  onChange: ChangeEventHandler<HTMLInputElement>
+  onCompositionStart: CompositionEventHandler<HTMLInputElement>;
+  onCompositionEnd: CompositionEventHandler<HTMLInputElement>;
+}
+
+function useCompositionInput(
+  callback: Callbacks["onChange"] | Callbacks["onCompositionEnd"],
+  deps: DependencyList
+): Callbacks {
+  const locked = useRef(false);
+  return useMemo(
+    () => ({
+      onChange: (e) => {
+        if (!locked) {
+          callback(e);
+        }
+      },
+      onCompositionStart: () => {
+        locked.current = true;
+      },
+      onCompositionEnd: (e) => {
+        locked.current = false;
+        callback(e)
+      },
+    }),
+    deps
+  );
+}
+
+export default useCompositionInput;
